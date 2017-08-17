82bfd9c6e30501b660915d7599f0d7d4489eea6f
usuario <usuario@com2pc05.uesocc.edu.sv>
2017-08-17 06:30:28 -0600

    se creé el método 3 y se modifircaron los demás métodos

--- src/main/java/com/mycompany/guia01/Utilities.java
+++ src/main/java/com/mycompany/guia01/Utilities.java
@@ -4,7 +4,7 @@ public class Utilities {
     /**
      * Este método hace un resumen de un texto , devolviendo los primeros 30 caracteres
      * @param texto Esta es la variable que recibirá un texto 
-     * @return Acá devolvera los primeros 30 caracteres de un texto
+     * @return Acá devolverá los primeros 30 caracteres de un texto
      */
     public String getResume(String texto){
          String txt= "";
@@ -22,12 +22,14 @@ public class Utilities {
     
     /**
      * ESte método Capitaliza la primera letra de las palabras de una frase
-     * @param texto 
-     * @return 
+     * @param texto Esta es la variable que recibirá un texto 
+     * @return devolverá las perimeras letras de un oracion en mayusculas y sin dobles espacios
      */
     public String capitalizar (String texto){
         
+        for(int esp=0; esp<texto.length(); esp++){
         texto=texto.replaceAll("  ", " ");
+        }
         char [] caracter= texto.toCharArray();
         for(int i=0; i<texto.length(); i++){
             if(caracter[i]==' ' || caracter[i]=='.' || caracter[i]==','){
@@ -39,7 +41,29 @@ public class Utilities {
         }
         return new String (caracter);
     }
-    
+    /**
+     * Este método debe de contar cuantas veces una palabra se repite en una oracion
+     * @param frase esta variable es la palabra a buscar
+     * @param texto este es la variable que recibirá un texto
+     * @return debolvera la cantidad de palabras repetidas
+     */
+    public int contarCoincidencias(String frase, String texto){
+        int cont=0, esp=0;
+        String encont;
+        if(frase.length()<texto.length()){
+            for(int pos=frase.length(); pos<=texto.length(); pos++, esp++){
+                encont="";
+                for(int poss=0+esp; poss<frase.length()+esp; poss++ ){
+                    encont=encont+String.valueOf(texto.charAt(poss));
+                }
+                if(frase.equalsIgnoreCase(encont)){
+                    cont=cont+1;
+                }
+            }
+        }
+      
+        return cont;
+    }
     
     
 }
